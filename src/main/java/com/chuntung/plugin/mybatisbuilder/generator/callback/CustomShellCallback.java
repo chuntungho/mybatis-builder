fbaf71438a24afdb30ec0dd74bae6597fe86f4bd
Tony Ho <ho@chuntung.com>
2019-06-17 00:02:26 +0800

    improve java merger to remove used import

--- src/main/java/com/chuntung/plugin/mybatisbuilder/generator/callback/CustomShellCallback.java
+++ src/main/java/com/chuntung/plugin/mybatisbuilder/generator/callback/CustomShellCallback.java
@@ -10,16 +10,18 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.Type;
 import org.mybatis.generator.config.MergeConstants;
 import org.mybatis.generator.exception.ShellException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.List;
+import java.util.*;
 
 /**
  * Custom shell callback to support auto merge.
@@ -27,6 +29,9 @@ import java.util.List;
  * @author Tony Ho
  */
 public class CustomShellCallback extends DefaultShellCallback {
+
+    private static final String JAVA_PREFIX = "java.";
+
     /**
      * Instantiates a new default shell callback.
      *
@@ -42,8 +47,7 @@ public class CustomShellCallback extends DefaultShellCallback {
     }
 
     @Override
-    public String mergeJavaFile(String newFileSource,
-                                File existingFile, String[] javadocTags, String fileEncoding)
+    public String mergeJavaFile(String newFileSource, File existingFile, String[] javadocTags, String fileEncoding)
             throws ShellException {
         // only support interface file
         if (!newFileSource.contains("public interface")) {
@@ -65,22 +69,20 @@ public class CustomShellCallback extends DefaultShellCallback {
     }
 
     public String mergerFile(CompilationUnit newCompilationUnit, CompilationUnit oldCompilationUnit) {
-        NodeList<ImportDeclaration> oldImports = oldCompilationUnit.getImports();
-        NodeList<ImportDeclaration> newImports = newCompilationUnit.getImports();
-
-        LinkedHashSet<ImportDeclaration> customImportSet = new LinkedHashSet<>(oldImports);
-        customImportSet.removeAll(newImports);
-        for (ImportDeclaration importDeclaration : customImportSet) {
-            newCompilationUnit.addImport(importDeclaration);
-        }
-
         List<MethodDeclaration> oldMethods = oldCompilationUnit.getType(0).getMethods();
         TypeDeclaration<?> typeDeclaration = newCompilationUnit.getType(0);
         List<MethodDeclaration> newMethods = typeDeclaration.getMethods();
 
-        LinkedHashSet<MethodDeclaration> customMethodSet = new LinkedHashSet<>(oldMethods);
-        customMethodSet.removeAll(newMethods);
-        for (MethodDeclaration methodDeclaration : customMethodSet) {
+        NodeList<ImportDeclaration> oldImports = oldCompilationUnit.getImports();
+        NodeList<ImportDeclaration> newImports = newCompilationUnit.getImports();
+
+        LinkedHashSet<ImportDeclaration> usedImports = new LinkedHashSet<>();
+        LinkedHashSet<ImportDeclaration> deltaImports = new LinkedHashSet<>(oldImports);
+        deltaImports.removeAll(newImports);
+
+        LinkedHashSet<MethodDeclaration> deltaMethods = new LinkedHashSet<>(oldMethods);
+        deltaMethods.removeAll(newMethods);
+        for (MethodDeclaration methodDeclaration : deltaMethods) {
             if (methodDeclaration.getComment().isPresent()) {
                 if (methodDeclaration.getComment().get().getContent().contains(MergeConstants.NEW_ELEMENT_TAG)) {
                     // skip generated methods
@@ -88,9 +90,98 @@ public class CustomShellCallback extends DefaultShellCallback {
                 }
             }
 
+            findOutUsedImport(methodDeclaration, deltaImports, usedImports);
+
             typeDeclaration.addMember(methodDeclaration);
+        }
+
+        // add custom used imports
+        if (!usedImports.isEmpty()) {
+            for (ImportDeclaration importDeclaration : usedImports) {
+                newCompilationUnit.addImport(importDeclaration);
+            }
+
+            // re-sort by name, jdk type at the end
+            newCompilationUnit.getImports().sort(new Comparator<ImportDeclaration>() {
+                @Override
+                public int compare(ImportDeclaration o1, ImportDeclaration o2) {
+                    String left = o1.getNameAsString();
+                    String right = o2.getNameAsString();
+                    if (left.startsWith(JAVA_PREFIX) && !right.startsWith(JAVA_PREFIX)) {
+                        return 1;
+                    } else if (!left.startsWith(JAVA_PREFIX) && right.startsWith(JAVA_PREFIX)) {
+                        return -1;
+                    } else {
+                        return left.compareTo(right);
+                    }
+                }
+            });
         }
 
         return newCompilationUnit.toString();
     }
+
+    // check the method to find out used imports from delta imports
+    private void findOutUsedImport(MethodDeclaration declaration, Set<ImportDeclaration> deltaImports, Set<ImportDeclaration> usedImports) {
+        Iterator<ImportDeclaration> iterator = deltaImports.iterator();
+        while (iterator.hasNext()) {
+            ImportDeclaration importDeclaration = iterator.next();
+            if (existsImport(importDeclaration, declaration)) {
+                iterator.remove();
+                usedImports.add(importDeclaration);
+            }
+        }
+    }
+
+    // check if the import is used for the method
+    private boolean existsImport(ImportDeclaration importDeclaration, MethodDeclaration methodDeclaration) {
+        String type = importDeclaration.getNameAsString();
+        String simpleType = type.substring(type.lastIndexOf('.') + 1);
+
+        Type returnType = methodDeclaration.getType();
+        if (typeIsImported(returnType, simpleType)) {
+            return true;
+        }
+
+        NodeList<Parameter> parameters = methodDeclaration.getParameters();
+        for (Parameter parameter : parameters) {
+            if (typeIsImported(parameter.getType(), simpleType)) {
+                return true;
+            }
+
+            NodeList<AnnotationExpr> annotations = parameter.getAnnotations();
+            for (AnnotationExpr annotation : annotations) {
+                if (annotation.getNameAsString().equals(simpleType)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    // check if the type or type arguments use the imported type
+    private boolean typeIsImported(Type type, String simpleTypeName) {
+        // ClassOrInterfaceType
+        if (type instanceof ClassOrInterfaceType) {
+            ClassOrInterfaceType concreteType = (ClassOrInterfaceType) type;
+            // type
+            if (concreteType.getName().asString().equals(simpleTypeName)) {
+                return true;
+            }
+
+            // type argument
+            if (concreteType.getTypeArguments().isPresent()) {
+                for (Type argType : concreteType.getTypeArguments().get()) {
+                    ClassOrInterfaceType concreteArgType = (ClassOrInterfaceType) argType;
+                    if (concreteArgType.getName().asString().equals(simpleTypeName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
 }
